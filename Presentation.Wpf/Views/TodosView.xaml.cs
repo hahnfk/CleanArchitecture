62c8897d211f04f22617f4d7a45ae8823b8115e9
Frank Hahn <frank.hahn@ines.ch>
2025-10-20 15:09:48 +0200

    Styles hinzugefügt

--- Presentation.Wpf/Views/TodosView.xaml.cs
+++ Presentation.Wpf/Views/TodosView.xaml.cs
@@ -1,7 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace CleanArchitecture.Presentation.Wpf.Views;
 public partial class TodosView : UserControl
 {
     public TodosView() => InitializeComponent();
+    private void TextBox_Loaded(object sender, RoutedEventArgs e)
+    {
+        var tb = sender as TextBox;
+        if (tb == null) return;
+        if (tb.Visibility == Visibility.Visible)
+        {
+            tb.Focus();
+            tb.SelectAll();
+        }
+    }
 }
+
